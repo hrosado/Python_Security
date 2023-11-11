1e21e33df9d61898861ac100a7b3a2bb712ad6f5
Hommy.Rosado <hommy.rosado@gmail.com>
2023-11-10 19:51:35 -0500

    Update.

---
+++ Chapter_01/list.py
@@ -0,0 +1,18 @@
+protocolList = []
+
+protocolList.append("ftp")
+
+protocolList.append("ssh")
+
+protocolList.append("smtp")
+
+protocolList.append("http")
+
+print(protocolList)
+
+protocolList.sort() # Alphabetical order
+print(protocolList)
+
+print(type(protocolList))
+print(len(protocolList))
+
