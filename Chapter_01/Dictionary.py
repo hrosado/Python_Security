61dd8c5e44b602b7f14a98b9e2c5df1d3c8ed365
Hommy Rosado <hommy.rosado@gmail.com>
2023-11-12 21:43:37 -0500

    Dictionaries.

---
+++ Chapter_01/Dictionary.py
@@ -0,0 +1,30 @@
+"""Dictionaries allow us to associate values with keys.
+The KEY is IMMUTABLE, but the value can be changed.
+Dictionaries are associative matrices."""
+
+
+# Dictionaries
+services = {"FTP":21,"HTTP":80,"SMTP":25,"SSH":22}
+services2 = {"FTP":21,"HTTP":80,"SNMP":161,"LDAP":389}
+
+services.update(services2)
+
+print(services)
+# services["FTP"] =8080
+print(services.keys())
+print(services.values())
+print(services.items())
+
+items = services.items()
+print(items)
+
+print(type(services))
+print(type(items))
+print(services.items())
+
+
+keys = services.keys()
+print(keys)
+services.has_key('http')
+
+
