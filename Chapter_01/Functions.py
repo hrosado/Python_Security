2b99c5d92998cf8c7935d34b9b7b49719a03fa69
Hommy Rosado <hommy.rosado@gmail.com>
2023-11-12 22:38:18 -0500

    Functions completed.

---
+++ Chapter_01/Functions.py
@@ -0,0 +1,23 @@
+"""Python Functions and managing exceptions."""
+
+# Example syntax in book doesnt provide a good example
+# def contains(sequence, items):
+#     for element in sequence:
+#         if element == item:
+#             return True
+#     return False
+
+# Managing exceptions
+# Divide by zero exception
+# try:
+#     print("[+] 10/0 =" + str(10/0))
+# except ValueError:
+#     print("Error = " + str(ValueError))
+
+# File not found exception
+# try:
+#     #f = file("file.txt") # depricated
+#     f = open("file.txt")
+# except ValueError:
+#     print("File not found = " + str(ValueError))
+
